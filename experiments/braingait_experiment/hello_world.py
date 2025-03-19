aca986c2c230e84e274c151d73d8f2acc07a30ed
DanielleLiu <10407817+Shuqi-Liu@users.noreply.github.com>
2025-03-19 14:34:52 -0400

    folder renaming + add in hello world for testing if python can be run in a new pc

---
+++ experiments/braingait_experiment/hello_world.py
@@ -0,0 +1,8 @@
+
+def main(subj_id):
+    print('hello world')
+
+
+if __name__ == "__main__":
+    main('subject-00') #need to have this as a default value to get us
+    # started, otherwise will error out
